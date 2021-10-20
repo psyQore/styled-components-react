a3d0c9fe8af3202e114f9ac0f7bfb83a56759a17
Juan Galarce <j6alarce@gmail.com>
2021-10-19 23:21:27 -0300

    Ejemplo de pseudo elemento, selectores y como agregar propiedades a los comÃponegar propiedades a los componentes

--- src/App.js
+++ src/App.js
@@ -11,11 +11,26 @@ const Content = styled.div`
 `;
 
 const Button = styled.button`
+  transition: box-shadow 0.2s ease;
   background-color: ${(props) => (props.primary ? "red" : "white")};
   color: ${(props) => (props.primary ? "white" : "red")};
   padding: 10px 15px;
   border: solid 2px red;
   border-radius: 4px;
+
+  &:hover {
+    box-shadow: 2px 2px 7px rgb(0, 0, 0, 0.6);
+  }
+
+  &.secondary {
+    background-color: blue;
+    border: solid 2px blue;
+    color: white;
+  }
+
+  .info {
+    font-size: 28px;
+  }
 `;
 
 const BlockButton = styled(Button)`
@@ -25,26 +40,47 @@ const BlockButton = styled(Button)`
 `;
 
 const Link = ({ className, ...props }) => {
-  return <a className={className} {...props}></a>;
+  return <a className={className} {...props} ></a>;
 };
 
 const StyledLink = styled(Link)`
   color: blue;
 `;
 
+const Input = styled.input.attrs((props) => ({
+  type: "text",
+  color: props.color || "red",
+}))`
+  font-size: 20px;
+  border: 1px solid red;
+  color: ${(props) => props.color};
+`;
+
+const Password = styled(Input).attrs({
+  type: 'password'
+})`
+
+`;
+
 const App = () => {
   return (
     <Content>
       <P>Hello World</P>
-      <Button>Enviar</Button>
+      <Button>
+        Enviar <p className='info'>Info</p>
+      </Button>
+      <Button className='secondary'>Enviar</Button>
       <Button primary>Enviar</Button>
       <BlockButton primary as='a' href='#'>
         Enviar
       </BlockButton>
       <BlockButton primary>Enviar</BlockButton>
-      <Link>Link</Link> <br />
+      <Link>Link</Link>
       <StyledLink>Link con Estilo</StyledLink>
+      <Input />
+      <Password />
     </Content>
   );
 };
+
 export default App;
