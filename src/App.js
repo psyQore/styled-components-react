6a892ce625c2bbc81ef57af3185388c49872096a
Juan Galarce <j6alarce@gmail.com>
2021-10-19 22:54:44 -0300

    Se crea ejemplo de como pasar props a los componente creados con styled-components

--- src/App.js
+++ src/App.js
@@ -10,10 +10,20 @@ const Content = styled.div`
   padding: 20px 25px;
 `;
 
+const Button = styled.button`
+  background-color: ${(props) => (props.primary ? "red" : "white")};
+  color: ${(props) => (props.primary ? "white" : "red")};
+  padding: 10px 15px;
+  border: solid 2px red;
+  border-radius: 4px;
+`;
+
 const App = () => {
   return (
     <Content>
       <P>Hello World</P>
+      <Button >Enviar</Button>
+      <Button primary>Enviar</Button>
     </Content>
   );
 };
